7edbd7f96a894f8ee4f950ba0e2c0d48ee21c04b
Luis Morenom <l.david1929@gmail.com>
2018-09-23 16:04:42 -0500

    register models in admin

--- nugis/music/admin.py
+++ nugis/music/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (Album,
+                     Gender,
+                     Artist,
+                     Track,
+                     TrackArtist,
+                     TrackGender,
+                     PlayList,
+                     PlayListTrack)
 
-# Register your models here.
+
+admin.site.register(Album)
+admin.site.register(Gender)
+admin.site.register(Artist)
+admin.site.register(Track)
+admin.site.register(TrackArtist)
+admin.site.register(TrackGender)
+admin.site.register(PlayList)
+admin.site.register(PlayListTrack)
