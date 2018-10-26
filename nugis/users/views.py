be061c6946cf97a60147613cf5d1fe6774d90e6b
Luis Moreno <l.david1929@gmail.com>
2018-10-25 22:05:06 -0500

    create, update and retrieve user

--- nugis/users/views.py
+++ nugis/users/views.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.models import User
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
+
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 
 
-class UserViewSet(ModelViewSet):
+class UserList(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserDetail(RetrieveUpdateAPIView):
     """
     doc for users
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
